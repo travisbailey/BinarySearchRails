ff66774a3e774eb2274073ecaea9ac9aaea6406d
David <david@provingground.net>
2012-11-06 14:11:21 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BinarySearch::Application.config.secret_token = 'e26888df8fbf00e43e5f7f0c4167edc179cc3235fce0f3427ce1fdc8c1b86f87901806749c7f6fff135de25d4aebe56292903ad06804d8163a4acd9656a4652a'
