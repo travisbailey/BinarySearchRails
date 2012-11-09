b3879c4d18f886c95fbff65cb5dc5366e892c411
David <david@provingground.net>
2012-11-09 14:11:14 -0500

    Changed home page and added form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BinarySearch::Application.routes.draw do
+  resources :microposts
+
   get "pages/home"
 
   get "pages/contact"
@@ -59,4 +61,6 @@ BinarySearch::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+root :to => 'pages#home'
 end
